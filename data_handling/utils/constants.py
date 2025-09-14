d2c71588501085d3bbaaa8f19c6b63709012e136
stevendindl <sdindl@email.sc.edu>
2025-09-14 13:44:10 -0400

    Last weeks updates to cleaning pipeline -- csv and streamlining

---
+++ data_handling/utils/constants.py
@@ -0,0 +1,11 @@
+"""
+Constants for file paths
+"""
+
+class Paths:
+    # Path locations for data access via notebooks
+    mccray_folder = r"../data/mccray/"
+
+    mccray_original_metadata = mccray_folder + r"original_data/McCray metadata.csv"
+    mccray_modified_metadata = mccray_folder + r"changed_data/McCray+.csv"
+
