4874d4fe565240f1ff5d02188fc6298a6407cbaa
stevendindl <sdindl@email.sc.edu>
2025-10-30 16:08:07 -0400

    Data standarization, cleaning, and metrics
    Work towards better metrics for data quality and improved data isolation and seperation
    
    Improved word checking for text strings
    
    Testing two different versions of the cleaning (original v1 and a more simple cleaning v2)
    
    csv files for standardized naming, still cleaning up and sorting out all previous csv and additional csv files I have locally

--- data_handling/utils/ocr_cleaning.py
+++ data_handling/utils/ocr_cleaning.py
@@ -26,12 +26,16 @@ class OCR_Clean:
     special_pattern = r'[^a-zA-Z0-9\s.,!?;:\'"()\-_/]{2,}'  # anything that's not letter, number, white space, or standard punctuation, in a sequence for 2 or more
     letter_pattern = r'\b\w*([a-zA-Z])\1{2,}\w*\b'          # sequences of 3+ of the same letter
 
-    # general_pattern = r'\b\S*[@#$%^&*+=<>\|\`~[\]{}]\S*\b'  # sequences with non-alpha numeric sandwiched between any other characters
     general_exceptions = [
-            r'^[a-zA-Z]{1,2}&[a-zA-Z]{1,2}$',
-            r'^[a-zA-Z]+=+[a-zA-Z]+$',
-            r'^[0-9]+\*[0-9]+$'
+            r'^[a-zA-Z]{1,2}&[a-zA-Z]{1,2}$',           # e.g., a&b, AB&CD
+            r'^[a-zA-Z]+=+[a-zA-Z]+$',                  # e.g., a=b, word=word
+            r'^[a-zA-Z0-9]+\*[a-zA-Z0-9]+$',             # e.g., 2*3, 10*20 or didn*t
+            # r"^[a-zA-Z]+\|-[a-zA-Z]+$"                   # e.g., word|-word 
         ]
+    '''
+    general_pattern example matches
+
+    '''
     general_pattern = r'\b(?!(?:\w{1,2}&\w{1,2}|\w+=\w+|\d+\*\d+)$)\S*[@#$%^&*+=<>\|\`~[\]{}]\S*\b'
 
     def compile():
