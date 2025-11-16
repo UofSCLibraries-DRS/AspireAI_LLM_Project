770895930c6867526dd54ef2afee5e1c7e0696a4
john <johnaaydin@gmail.com>
2025-11-16 08:45:48 -0500

    Add debug prints in test script

--- fine_tuning/test.py
+++ fine_tuning/test.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 
-from fine_tuning.utils.parse_pipeline import build_pipeline
-from fine_tuning.inference import batched_inference
+# from fine_tuning.utils.parse_pipeline import build_pipeline
+# from fine_tuning.inference import batched_inference
 from fine_tuning.trainers import LoRAUnsupervisedTrainer
 
 
@@ -12,14 +12,16 @@ PIPELINE_PATH = (
 
 
 def main():
-    load_dotenv(find_dotenv(".env.local"))
+    # load_dotenv(find_dotenv(".env.local"))
 
-    pipeline = build_pipeline(
-        pipeline_path=PIPELINE_PATH,
-    )
+    # pipeline = build_pipeline(
+    #     pipeline_path=PIPELINE_PATH,
+    # )
 
-    for train_step in pipeline.train_steps:
-        train_step.train()
+    # for train_step in pipeline.train_steps:
+    #     train_step.train()
+
+    print("Here 1")
 
     trainer = LoRAUnsupervisedTrainer(
         start_model="/work/jaaydin/models/gemma-3-270m",
@@ -27,6 +29,8 @@ def main():
         data="/work/jaaydin/raw/100_transcript.csv",
         config="/work/jaaydin/AspireAI_LLM_Project/fine_tuning/config/ft/training/lora_unsupervised_D_mcc_h2.json",
     )
+
+    print("Here 2")
 
     trainer.train()
 
