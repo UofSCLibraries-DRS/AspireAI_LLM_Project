364fb5dec972b179983ba58f09a47d2ed9beacbb
john <johnaaydin@gmail.com>
2025-12-08 10:56:31 -0500

    Fix imports in fine_tuning/trainers/__init__.py

--- fine_tuning/trainers/__init__.py
+++ fine_tuning/trainers/__init__.py
@@ -3,10 +3,12 @@ from .training_base import AbstractTrainer
 from .full_unsupervised import FullUnsupervisedTrainer
 from .lora_unsupervised import LoRAUnsupervisedTrainer
 from .full_sft import FullSFTTrainer
+from .lora_sft import LoRASFTTrainer
 
 __all__ = [
     "AbstractTrainer",
     "FullUnsupervisedTrainer",
     "LoRAUnsupervisedTrainer",
     "FullSFTTrainer",
+    "LoRASFTTrainer",
 ]
