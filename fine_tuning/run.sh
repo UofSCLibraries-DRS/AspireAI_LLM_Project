cb1a4657f4c861624d3ac376db80d71ca640153b
john <johnaaydin@gmail.com>
2025-12-19 16:24:10 -0500

    Fix C compiler incompatibility on L40s nodes

--- fine_tuning/run.sh
+++ fine_tuning/run.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:1   ## Run on 1 GPU
 #SBATCH --output logs/lora_ft_%j.out
 #SBATCH --error logs/lora_ft_%j.err
-#SBATCH -p AI_Center_L40S,gpu-v100-32gb
+#SBATCH -p AI_Center_L40S
 
 export CUDA_VISIBLE_DEVICES=0
 
@@ -13,13 +13,18 @@ cd /work/jaaydin/AspireAI_LLM_Project
 
 module load cuda/12.1
 
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+
 source ~/miniforge3/etc/profile.d/conda.sh
 
 conda activate lib_train
 
+
 python -u -m fine_tuning.main \
-    --pipeline-path ./fine_tuning/config/llama_pipeline.json \
+    --pipeline-path /work/jaaydin/AspireAI_LLM_Project/fine_tuning/config/pipelines/llama/llama_pipeline.json \
     --env .env.rci
 
 # conda run -p /home/jaaydin/.conda/envs/test_env python -m fine_tuning.main --pipeline-path ./fine_tuning/config/llama_pipeline.json --env .env.rci
 # python -m fine_tuning.main --pipeline-path ./fine_tuning/config/llama_pipeline.json --env .env.rci
+
+
