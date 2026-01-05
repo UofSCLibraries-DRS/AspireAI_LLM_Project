5a19b42200511653ffe003485baff509d04b609f
john <johnaaydin@gmail.com>
2026-01-05 08:13:32 -0500

    Add missing training steps, fix env issues

--- fine_tuning/run.sh
+++ fine_tuning/run.sh
@@ -1,30 +1,23 @@
 #!/bin/sh
 #SBATCH --job-name=library_ft
 #SBATCH -N 1
-#SBATCH -n 16    ##24 cores(of 48) so you get 1/2 of machine RAM ( 192 GB total)
-#SBATCH --gres=gpu:1   ## Run on 1 GPU
+#SBATCH -n 16
+#SBATCH --gres=gpu:1
 #SBATCH --output logs/lora_ft_%j.out
 #SBATCH --error logs/lora_ft_%j.err
 #SBATCH -p AI_Center_L40S
 
 export CUDA_VISIBLE_DEVICES=0
-
 cd /work/jaaydin/AspireAI_LLM_Project
+
+source ~/miniforge3/etc/profile.d/conda.sh
+conda activate lib_train
 
 module load cuda/12.1
 
+# Force conda's libstdc++ to be used
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
-source ~/miniforge3/etc/profile.d/conda.sh
-
-conda activate lib_train
-
-
 python -u -m fine_tuning.main \
-    --pipeline-path /work/jaaydin/AspireAI_LLM_Project/fine_tuning/config/pipelines/llama/llama_pipeline.json \
+    --pipeline-path /work/jaaydin/AspireAI_LLM_Project/fine_tuning/config/pipelines/llama/llama_full.json \
     --env .env.rci
-
-# conda run -p /home/jaaydin/.conda/envs/test_env python -m fine_tuning.main --pipeline-path ./fine_tuning/config/llama_pipeline.json --env .env.rci
-# python -m fine_tuning.main --pipeline-path ./fine_tuning/config/llama_pipeline.json --env .env.rci
-
-
