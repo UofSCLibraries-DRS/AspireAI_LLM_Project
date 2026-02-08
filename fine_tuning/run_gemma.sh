9ed5b505963b532ed5b30aa47d608e199a76fbe1
John-A-Aydin <johnaaydin@gmail.com>
2026-02-08 15:08:13 -0500

    Add unified gemma pipline, refactor Llama pipline to support new inference file

--- fine_tuning/run_gemma.sh
+++ fine_tuning/run_gemma.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 #SBATCH --job-name=library_ft
 #SBATCH -N 1
-#SBATCH -n 16    ##24 cores(of 48) so you get 1/2 of machine RAM ( 192 GB total)
-#SBATCH --gres=gpu:1   ## Run on 1 GPU
+#SBATCH -n 16
+#SBATCH --gres=gpu:1
 #SBATCH --output logs/lora_ft_%j.out
 #SBATCH --error logs/lora_ft_%j.err
-#SBATCH -p AI_Center_L40S,gpu-v100-32gb
+#SBATCH -p AI_Center_L40S
 
 export CUDA_VISIBLE_DEVICES=0
-
 cd /work/jaaydin/AspireAI_LLM_Project
+
+source ~/miniforge3/etc/profile.d/conda.sh
+conda activate lib_train
 
 module load cuda/12.1
 
-source ~/miniforge3/etc/profile.d/conda.sh
-
-conda activate lib_train
+# Force conda's libstdc++ to be used
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
 python -u -m fine_tuning.main \
-    --pipeline-path ./fine_tuning/config/gemma_pipeline.json \
+    --pipeline-path /work/jaaydin/AspireAI_LLM_Project/fine_tuning/config/pipelines/gemma/gemma_full.json \
     --env .env.rci
