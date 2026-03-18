eae97cdaee4f1a20e11e0c11f5e7a1e42331ec74
john <johnaaydin@gmail.com>
2026-03-18 11:48:38 -0400

    Update Llama LoRA rank 64 training config

---
+++ fine_tuning/run_h200.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#SBATCH --job-name=library_ft
+#SBATCH -N 1                   # 1 node
+#SBATCH -n 16                  # 16 CPU tasks (threads)
+#SBATCH --gres=gpu:2           # request 4 A100 GPUs
+#SBATCH --output=logs/lora_ft_%j.out
+#SBATCH --error=logs/lora_ft_%j.err
+#SBATCH -p gpu-H200
+#SBATCH --time=24:00:00
+
+# Navigate to project directory
+cd /work/jaaydin/AspireAI_LLM_Project
+
+# Load conda
+source /work/jaaydin/miniconda3/etc/profile.d/conda.sh
+conda activate lib_train
+
+# Load CUDA module
+module load cuda/12.1
+
+# Force conda's libstdc++ to be used (avoid conflicts)
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+
+# Optional: set TMPDIR for large pip wheels or temp files
+export TMPDIR=/work/jaaydin/tmp
+
+export PYTHONPATH="$(pwd):$PYTHONPATH"
+
+# Optional: use Accelerate for multi-GPU training
+# Make sure your training script supports Accelerate
+accelerate launch \
+    --multi_gpu \
+    --mixed_precision bf16 \
+    --num_processes 2 \
+    fine_tuning/main.py \
+    --pipeline-path /work/jaaydin/AspireAI_LLM_Project/fine_tuning/config/pipelines/llama/llama_cpt_lora64.json \
+    --env .env.rci
